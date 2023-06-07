e32f9720711e49fd66d161fb340d8f0a9a041fc2
Yosef sides <yosefsides1@gimail.com>
2023-06-07 19:12:49 +0300

    add_img

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import img from "./Image.png"
 function Home() {
   return (
     <div>
-        
+       <img src={img} alt="img_top" />
+       <h1>The Nature Spot</h1> 
+      <Categories />
     </div>
   )
 }
