25fdb170c723a8f788ffafeceb0b09ca9fdc3fad
unknown <dvd3418@gmail.com>
2023-06-07 19:31:34 +0300

    add pageOne to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Page_one from "./pageOne";
 
 function App() {
     <div>
+        <h1>App</h1>
         <Page_one />
     </div>
 
--- src/pageOne/index.jsx
+++ src/pageOne/index.jsx
@@ -2,7 +2,8 @@ import styles from './style.module.css'
 export default function Page_one() {
     return (
         <div className={styles.div}>
-            <img className={styles.imgBack} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY1dSA-z8lLUz1gRKkAD1aACtc3QmW0cVtPPXXGKqkDNBt3xtW8XAc1yzry6YcovVjY3c&usqp=CAU" />
+            <h1>helllooo</h1>
+            <img className={styles.imgBack} ref="src/pageOne/Image.png" />
         </div>
     )
 }
